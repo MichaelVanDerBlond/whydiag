443b7359eb48af438a2d9c082464d0b3f7e9bfd7
MichaelVanDerBlond <ub6gtx@gmail.com>
2026-07-15 17:42:36 +0300

    feat(cli): add basic command line interface

--- cmd/whydiag/main.go
+++ cmd/whydiag/main.go
@@ -5,13 +5,39 @@ import (
 	"os"
 
 	"github.com/MichaelVanDerBlond/whydiag/internal/app"
+	"github.com/MichaelVanDerBlond/whydiag/internal/version"
 )
 
-func main() {
-	a := app.New()
+func usage() {
+	fmt.Println("WhyDiag")
+	fmt.Println()
+	fmt.Println("Usage:")
+	fmt.Println("  whydiag check")
+	fmt.Println("  whydiag version")
+}
 
-	if err := a.Run(); err != nil {
-		fmt.Fprintln(os.Stderr, err)
+func main() {
+
+	if len(os.Args) < 2 {
+		usage()
+		os.Exit(1)
+	}
+
+	switch os.Args[1] {
+
+	case "version":
+		fmt.Printf("%s %s\n", version.Name, version.Version)
+
+	case "check":
+		a := app.New()
+
+		if err := a.Run(); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+
+	default:
+		usage()
 		os.Exit(1)
 	}
 }
